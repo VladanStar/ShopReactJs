3811a10c025fff9c7501f67d6b4da20c2ada743b
VladanStar <vladancupric@gmail.com>
2022-09-21 15:49:12 +0200

    Add footer and

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,42 +2,35 @@ import React from 'react'
 
 const Footer = () => {
   return (
-      <footer className="py-5 bg-dark ">
-          <div className="container  ">
-              <div className="row ">
-                  <div className="col-md-3 col-ms-6">
-                      <h4 id="white">About</h4>
-                      <p id="whiteP">
-                          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
-                          distinctio!. Lorem ipsum dolor sit amet consectetur, adipisicing
-                          elit. Sequi, distinctio
-                      </p>
+      <footer className="py-5 bg-dark text-white">
+          <div className="container">
+              <div className="row">
+                  <div className="col-md-3 col-sm-6">
+                      <h4 className="mb-4">About</h4>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veniam a numquam aliquam itaque placeat!</p>
                   </div>
-                  <div className="col-md-3 col-ms-6">
-                      <h4 id="white">Top products</h4>
+                  <div className="col-md-3 col-sm-6">
+                      <h4 className="mb-4">Top products</h4>
                       <ul className="list-unstyled">
-                          <li><a href="#">Jeans</a></li>
-                          <li><a href="#">Bags</a></li>
-                          <li><a href="#">Accesors</a></li>
-                          <li><a href="#">Phones</a></li>
+                          <li><a href="">Jeans</a></li>
+                          <li><a href="">Bags</a></li>
+                          <li><a href="">Accesories</a></li>
+                          <li><a href="">Phones</a></li>
                       </ul>
                   </div>
-                  <div classNameName="col-md-3 col-ms-6">
-                      <h4 id="white">Quick Links</h4>
-                      <ul classNameName="list-unstyled">
-                          <li><a href="#">Home</a></li>
-                          <li><a href="#">Blog</a></li>
-                          <li><a href="#">Shop</a></li>
-                          <li><a href="#">Contact</a></li>
+                  <div className="col-md-3 col-sm-6">
+                      <h4 className="mb-4">Quick links</h4>
+                      <ul className="list-unstyled">
+                          <li><a href="">Home</a></li>
+                          <li><a href="">Blog</a></li>
+                          <li><a href="">Shop</a></li>
+                          <li><a href="">Kontakt</a></li>
                       </ul>
                   </div>
-                  <div classNameName="col-md-3 col-ms-6">
-                      <a classNameName="" href="index.html">
-                          <img style={{ width: '160px' }}
-                              classNameName=" img-fluid rounded shadow-lg border border-white border-5 md-4"
-                              src="./img/logo.png"
-                              alt="..."
-                          /></a>
+                  <div className="col-md-3 col-sm-6">
+                      <a href="index.html">
+                          <img src="img/logo.png" alt="" className="img-fluid rounded rounded-3 shadow-lg border border-white border-5" />
+                      </a>
                   </div>
               </div>
           </div>
