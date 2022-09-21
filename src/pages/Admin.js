8e50554dbe823e86774c0ab944ec8d9825693c7e
VladanStar <vladancupric@gmail.com>
2022-09-21 21:22:32 +0200

    Add admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+      <section className='single py-5'>
+          <article className="container">
+              <div className="row align-items-center">
+                  <div className="col-md-10 mx-auto">
+                      <table className="table table-striped table-hover shadow-lg">
+                          <thead>
+                              <tr>
+                                  <th scope="col">#</th>
+                                  <th scope="col">Products</th>
+                                  <th scope="col">Quantity</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr>
+                                  <th scope="row">1</th>
+                                  <td>Orange T-shirt</td>
+                                  <td>1</td>
+                              </tr>
+                              <tr>
+                                  <th scope="row">2</th>
+                                  <td>traveller Bag</td>
+                                  <td>1</td>
+                              </tr>
+                              <tr>
+                                  <th scope="row">3</th>
+                                  <td>Sunglasses</td>
+                                  <td>1</td>
+                              </tr>
+                          </tbody>
+                          
+                      </table>
+                      <Link to="/" className="button mt-4">Paymen Proces</Link>
+                      
+                  </div>
+                  
+              </div>
+              
+          </article>
+          
+    </section>
   )
 }
 
