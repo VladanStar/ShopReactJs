171db637c0f457d8f789884e2e2ab28f42702479
VladanStar <vladancupric@gmail.com>
2022-09-21 15:54:49 +0200

    Add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,64 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-      <footer className="py-5 bg-dark text-white">
-          <div className="container">
-              <div className="row">
-                  <div className="col-md-3 col-sm-6">
-                      <h4 className="mb-4">About</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veniam a numquam aliquam itaque placeat!</p>
-                  </div>
-                  <div className="col-md-3 col-sm-6">
-                      <h4 className="mb-4">Top products</h4>
-                      <ul className="list-unstyled">
-                          <li><a href="">Jeans</a></li>
-                          <li><a href="">Bags</a></li>
-                          <li><a href="">Accesories</a></li>
-                          <li><a href="">Phones</a></li>
-                      </ul>
-                  </div>
-                  <div className="col-md-3 col-sm-6">
-                      <h4 className="mb-4">Quick links</h4>
-                      <ul className="list-unstyled">
-                          <li><a href="">Home</a></li>
-                          <li><a href="">Blog</a></li>
-                          <li><a href="">Shop</a></li>
-                          <li><a href="">Kontakt</a></li>
-                      </ul>
-                  </div>
-                  <div className="col-md-3 col-sm-6">
-                      <a href="index.html">
-                          <img src="img/logo.png" alt="" className="img-fluid rounded rounded-3 shadow-lg border border-white border-5" />
-                      </a>
-                  </div>
-              </div>
+    <footer className="py-5 bg-dark text-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <h4 className="mb-4">About</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              veniam a numquam aliquam itaque placeat!
+            </p>
           </div>
-      </footer>
-  )
-}
+          <div className="col-md-3 col-sm-6">
+            <h4 className="mb-4">Top products</h4>
+            <ul className="list-unstyled">
+              <li>
+                <a href="">Jeans</a>
+              </li>
+              <li>
+                <a href="">Bags</a>
+              </li>
+              <li>
+                <a href="">Accesories</a>
+              </li>
+              <li>
+                <a href="">Phones</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4 className="mb-4">Quick links</h4>
+            <ul className="list-unstyled">
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Blog</a>
+              </li>
+              <li>
+                <a href="">Shop</a>
+              </li>
+              <li>
+                <a href="">Kontakt</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <a href="index.html">
+              <img
+                src="img/logo.png"
+                alt=""
+                className="img-fluid rounded rounded-3 shadow-lg border border-white border-5"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
