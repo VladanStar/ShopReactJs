882e9cd44bd0f6c1be2e6e27ccbbcd696127b981
VladanStar <vladancupric@gmail.com>
2022-09-21 21:07:15 +0200

    add

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <secttion className="single py-5">
+      <article className="container">
+        <div className="row align-items-center">
+          <div className="col-md-10 mx-auto">
+            <table className="table table-striped table-hover shadow-lg">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Product</th>
+                  <th scope="col">Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Orange t-shirt</td>
+                  <td>1</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Traveller bag</td>
+                  <td>1</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Sunglasses</td>
+                  <td>1</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </article>
+    </secttion>
+  );
+};
 
-export default Cart
+export default Cart;
