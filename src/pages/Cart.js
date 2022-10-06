aaa2a9a4aebfda47b487332ab0227745228743eb
unknown <vladancupric@gmail.com>
2022-10-06 08:31:48 +0200

    add

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -14,14 +14,18 @@ const Cart = ({cart, setCart}) => {
                   <th scope="col">image</th>
                   <th scope="col">Title</th>
                   <th scope="col">Quantity</th>
-                  <th scope="col">)Price</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Description</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <th scope="row">1</th>
+                  <td><img src="img/product1.jpg"/></td>
                   <td>Orange t-shirt</td>
                   <td>1</td>
+                  <td>$39</td>
+                  <td>Neki opis</td>
                 </tr>
                 <tr>
                   <th scope="row">2</th>
